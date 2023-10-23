c1bdaa657e301a11b07e30186650257a41996853
nael <ouldbelkacemnael@yahoo.fr>
2023-10-23 23:17:56 +0200

    feat: panora docs openapi

--- api/src/main.ts
+++ api/src/main.ts
@@ -1,8 +1,16 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  const config = new DocumentBuilder()
+    .setTitle('Unified Panora API')
+    .setDescription('The Panora API description')
+    .setVersion('1.0')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('docs', app, document);
   await app.listen(3000);
 }
 bootstrap();
