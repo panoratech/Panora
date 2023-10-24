ccc918c6fd445337fa17afd67b510ae6a0c1770a
nael <ouldbelkacemnael@yahoo.fr>
2023-10-24 20:51:33 +0200

    feat: added husky rules

--- api/src/app.module.ts
+++ api/src/app.module.ts
@@ -1,6 +1,7 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ContactModule } from './crm/contact/contact.module';
 import { CrmModule } from './crm/crm.module';
 
 @Module({
