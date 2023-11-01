1dcf985cef8e830f519b2285a41ef1752c9ae2d8
nael <ouldbelkacemnael@yahoo.fr>
2023-11-01 15:51:54 +0100

    feat: pull database script

--- packages/api/prisma/migrations/20231023135023_init/migration.sql
+++
@@ -1,7 +0,0 @@
--- CreateTable
-CREATE TABLE "Crm" (
-    "id" SERIAL NOT NULL,
-    "name" TEXT,
-
-    CONSTRAINT "Crm_pkey" PRIMARY KEY ("id")
-);
---
+++ packages/api/scripts/pull-database.sh
@@ -0,0 +1,9 @@
+# SCRIPT TO USE IF YOU WANT TO UPDATE YOUR PRISMA MODEL BASED ON YOUR DB 
+# it pulls changes from your database to your Prisma schema 
+# DB => PRISMA MODEL 
+
+# Check if there are any new migrations and run them.
+npx prisma db pull
+
+# Generate Prisma Client
+npx prisma generate
