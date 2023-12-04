4ee9338ed47d880b628219cf3b8fd46f774df8d4
nael <ouldbelkacemnael@yahoo.fr>
2023-12-04 16:10:44 +0100

    :bug: Fix jobs service

--- packages/api/src/@core/jobs/jobs.module.ts
+++ packages/api/src/@core/jobs/jobs.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { JobsService } from './jobs.service';
 import { JobsController } from './jobs.controller';
+import { LoggerService } from '@@core/logger/logger.service';
+import { PrismaService } from '@@core/prisma/prisma.service';
 
 @Module({
-  providers: [JobsService],
+  providers: [JobsService, LoggerService, PrismaService],
   controllers: [JobsController],
 })
 export class JobsModule {}
