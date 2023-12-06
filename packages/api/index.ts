f5fd5b4cad66e76c15ad2605d3eac0357b41dde9
nael <ouldbelkacemnael@yahoo.fr>
2023-12-06 19:48:20 +0100

    :bug: Fix ci

--- packages/api/index.ts
+++ packages/api/index.ts
@@ -1,3 +1,3 @@
 export * from '@prisma/client';
 //export * from './src/@core/utils/types';
-//export { getProviderVertical } from '@@core/utils/providers';
+//export { getProviderVertical } from './src/@core/utils/providers';
