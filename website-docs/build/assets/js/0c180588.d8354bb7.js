c7551ab9db03be99ddc06226f8048ed4944543de
Rachid F <rf@artesi.xyz>
2023-12-30 12:33:32 +0100

    :bug: Docker: build all images from one file

--- website-docs/build/assets/js/0c180588.d8354bb7.js
+++
@@ -1,1 +0,0 @@
-"use strict";(self.webpackChunkwebsite_docs=self.webpackChunkwebsite_docs||[]).push([[214],{3769:e=>{e.exports=JSON.parse('{"name":"docusaurus-plugin-content-docs","id":"default"}')}}]);
