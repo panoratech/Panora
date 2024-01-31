e418a1a3adef2bb590ce03922cf330abf0c6aec4
nael <ouldbelkacemnael@yahoo.fr>
2024-01-31 09:46:37 +0100

    :bug: Fixed bugs

--- apps/embedded-catalog/react/src/lib/index.tsx
+++ apps/embedded-catalog/react/src/lib/index.tsx
@@ -1,38 +1,10 @@
 import config from '@/helpers/config';
-import { findProviderByName } from '@/helpers/utils';
+import { getDescription } from '@/helpers/utils';
 import useLinkedUserMutation from '@/hooks/mutations/useLinkedUserMutation';
 import useLinkedUser from '@/hooks/queries/useLinkedUserId';
 import useOAuth from '@/hooks/useOAuth';
 import { useEffect, useState } from 'react';
-import { TailSpin } from  'react-loader-spinner'
 
-const LoadingOverlay = ({ providerName }: { providerName: string }) => {
-  const provider = findProviderByName(providerName);
-  return (
-    <div className="fixed inset-0 flex justify-center items-center">
-      <div className="text-center p-6 bg-[#1d1d1d] rounded-lg">
-        <div className='icon-wrapper'>
-          <img src={provider!.logoPath} alt={provider!.name} className="mx-auto mb-4 w-14 h-14 rounded-xl" />
-        </div>
-        
-        <h4 className="text-lg font-bold mb-2">Continue in {provider!.name}</h4>
-        <p className="text-gray-400 mb-4">Accepting oAuth access to Panora</p>
-        <div className='flex justify-center items-center'>
-          <TailSpin
-            height="40"
-            width="40"
-            color="white"
-            ariaLabel="tail-spin-loading"
-            radius="1"
-            wrapperStyle={{}}
-            wrapperClass=""
-            visible={true}
-          />
-         </div>
-      </div>
-    </div>
-  );
-};
 
 interface RemoteUserInfo {
   userIdInYourSystem: string;
@@ -46,9 +18,9 @@ interface ProviderCardProp {
 
 }
 const ProviderCard = ({name, projectId, linkedUserIdOrRemoteUserInfo}: ProviderCardProp) => {
-  const [loading, setLoading] = useState(false);
+  const [providerClicked, setProviderClicked] = useState(false);
   const [originId, setOriginId] = useState("")
-
+  
   const { mutate } = useLinkedUserMutation();
   const {data: linkedUser} = useLinkedUser(originId);
 
@@ -69,7 +41,7 @@ const ProviderCard = ({name, projectId, linkedUserIdOrRemoteUserInfo}: ProviderC
   }
 
   const { open, isReady } = useOAuth({
-    providerName: name,
+    providerName: name.toLowerCase(),
     returnUrl: config.ML_FRONTEND_URL, // TODO: Replace with the actual return URL
     projectId: projectId,
     linkedUserId: linkedUserId,
@@ -77,45 +49,41 @@ const ProviderCard = ({name, projectId, linkedUserIdOrRemoteUserInfo}: ProviderC
   });
 
   const onWindowClose = () => {
-    setLoading(false)
   }
 
   useEffect(() => {
-    if (name && isReady) {
-        open(onWindowClose);
+    if (providerClicked && isReady) {
+      open(onWindowClose);
     }
-    
-  }, [name, isReady, open]);
+  }, [providerClicked, isReady, open]);
   
 
-  const handleClick = () => {
-    setLoading(true);
+  const handleClick = () => {    
+    setProviderClicked(true);
   };
     
   return (
-    <>
-    {!loading ? 
-    <div 
-    className="max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700"
-    onClick={() => handleClick()}
-    >
-        <div className="text-center flex items-center">
-          <img src={`public/assets/crm/${name}_logo.png`} width={"50px"} className="pb-5 mr-3 rounded-sm"/>
-          <a href="#">
-            <h5 className="mb-2 text-2xl font-semibold tracking-tight text-gray-900 dark:text-white">Integrate with {name}</h5>
-        </a>
-        </div>
-        
-        <p className="mb-3 font-normal text-gray-500 dark:text-gray-400">Go to this step by step guideline process on how to certify for your weekly benefits:</p>
-        <a href="#" className="inline-flex items-center text-blue-600 hover:underline">
-            See our guideline
-            <svg className="w-3 h-3 ms-2.5 rtl:rotate-[270deg]" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 18 18">
-                <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M15 11v4.833A1.166 1.166 0 0 1 13.833 17H2.167A1.167 1.167 0 0 1 1 15.833V4.167A1.166 1.166 0 0 1 2.167 3h4.618m4.447-2H17v5.768M9.111 8.889l7.778-7.778"/>
-            </svg>
-        </a>
-    </div> : 
-      <LoadingOverlay providerName={name}/> }
-    </>
+      <div 
+      className="max-w-sm p-6 bg-white border-[0.007em] border-gray-200 rounded-lg shadow dark:bg-zinc-800 hover:border-zinc-900 hover:border-[0.1em] transition-colors duration-200"
+      >
+          <div className="text-center flex items-center">
+            <img src={`public/assets/crm/${name}_logo.png`} width={"35px"} className="pb-5 mr-3 rounded-sm"/>
+            <a href="#">
+              <h5 className="mb-2 text-2xl font-semibold tracking-tight text-gray-900 dark:text-white">Integrate with {name}</h5>
+          </a>
+          </div>
+          
+          <p className="mb-3 font-normal text-gray-500 dark:text-gray-400">{getDescription(name.toLowerCase())}</p>
+          <a 
+            href="#" className="inline-flex items-center text-indigo-600 hover:underline"
+            onClick={handleClick}
+          >
+              Integrate in one click
+              <svg className="w-3 h-3 ms-2.5 rtl:rotate-[270deg]" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 18 18">
+                  <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M15 11v4.833A1.166 1.166 0 0 1 13.833 17H2.167A1.167 1.167 0 0 1 1 15.833V4.167A1.166 1.166 0 0 1 2.167 3h4.618m4.447-2H17v5.768M9.111 8.889l7.778-7.778"/>
+              </svg>
+          </a>
+      </div> 
   )
 };
   
