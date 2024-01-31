9ebbba2657589c6c4a34b353cc2950b6b808b159
nael <ouldbelkacemnael@yahoo.fr>
2024-01-31 03:52:29 +0100

    :sparkles: Making the catalog for embedded in tegration connections

---
+++ apps/embedded-catalog/react/src/App.tsx
@@ -0,0 +1,17 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <div>
+        <h1 className="text-3xl font-bold underline">
+        Hello world!
+        </h1>
+      </div>
+      
+    </>
+  )
+}
+
+export default App
