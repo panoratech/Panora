27d2e19f57e4e94823746ed865edc46d33d6c943
nael <ouldbelkacemnael@yahoo.fr>
2024-02-13 07:33:16 +0100

    :bug: Fix

---
+++ .changeset/lemon-chicken-enjoy.md
@@ -0,0 +1,5 @@
+---
+"@panora/embedded-card-react": patch
+---
+
+little fix
--- apps/embedded-catalog/react/src/helpers/config.ts
+++ apps/embedded-catalog/react/src/helpers/config.ts
@@ -1,5 +1,5 @@
 const config = {
-    API_URL: "http://localhost:3000"
+    API_URL: "https://api-dev.panora.dev" //"http://localhost:3000"
 };
   
 export default config;
