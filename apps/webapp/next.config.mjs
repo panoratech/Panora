6bd27c12da5744db590fd91251ad6ca2f8c4f4f4
nael <ouldbelkacemnael@yahoo.fr>
2024-03-11 12:02:09 +0100

    :bug: Remove webapp

--- apps/webapp/next.config.mjs
+++
@@ -1,7 +0,0 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-    output: 'export', // Outputs a Single-Page Application (SPA).
-    distDir: './dist', // Changes the build output directory to `./dist/`.
-}
-
-export default nextConfig
--- apps/webapp/src/components/auth/login/login-schema.ts
+++
@@ -1,9 +0,0 @@
-import { z } from 'zod';
-import { registerSchema } from '../register/register-schema';
-
-export const loginSchema = registerSchema.pick({
-  email: true,
-  password: true,
-});
-
-export type LoginSchemaType = z.infer<typeof loginSchema>;
