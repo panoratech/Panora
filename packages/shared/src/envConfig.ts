8734f552af215780cb8ead69916bdab929b86d64
nael <ouldbelkacemnael@yahoo.fr>
2024-04-12 18:15:01 +0200

    :pencil2: Fix

--- packages/shared/src/envConfig.ts
+++ packages/shared/src/envConfig.ts
@@ -92,7 +92,7 @@ export function needsSubdomain(provider: string, vertical: string): boolean {
     const providerConfig = providersConfig[vertical][provider];
    
     // Check if authBaseUrl and apiUrl start with a '/'
-    const authBaseUrlStartsWithSlash = providerConfig.authBaseUrl.startsWith('/');
+    const authBaseUrlStartsWithSlash = providerConfig.authBaseUrl!.startsWith('/');
     const apiUrlStartsWithSlash = providerConfig.apiUrl!.startsWith('/');
     const apiUrlIsBlank = providerConfig.apiUrl! == "";
     
