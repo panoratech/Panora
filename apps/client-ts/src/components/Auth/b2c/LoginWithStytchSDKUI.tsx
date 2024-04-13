88aec58ec47cc37707d56609beb40ce9313e38f8
nael <ouldbelkacemnael@yahoo.fr>
2024-04-13 02:13:46 +0200

    :sparkles: Add stytch

--- apps/client-ts/src/components/Auth/b2c/LoginWithStytchSDKUI.tsx
+++ apps/client-ts/src/components/Auth/b2c/LoginWithStytchSDKUI.tsx
@@ -4,6 +4,7 @@ import { StytchLogin } from '@stytch/nextjs';
 import { StytchLoginConfig, OAuthProviders, Products, StyleConfig, StytchEvent, StytchError } from '@stytch/vanilla-js';
 import { getDomainFromWindow } from '@/lib/stytch/urlUtils';
 import { Button } from '@/components/ui/button';
+import { Icons } from '@/components/shared/icons';
 
 const sdkStyle: StyleConfig = {
   fontFamily: '"Helvetica New", Helvetica, sans-serif',
@@ -31,18 +32,47 @@ const callbackConfig = {
   onError: (error: StytchError) => console.log(error),
 }
 
+const getOauthUrl = (provider: string) => {
+  const isTest = process.env.NEXT_PUBLIC_STYTCH_PROJECT_ENV == 'test';
+  const baseStytch = isTest ? "test" : "api"
+  return `https://${baseStytch}.stytch.com/v1/public/oauth/${provider.toLowerCase()}/start?public_token=${process.env.NEXT_PUBLIC_STYTCH_PUBLIC_TOKEN}`;
+}
+
 const LoginWithStytchSDKUI = () => {
   return (
     <>
-    <div className='flex flex-row mb-2'>
-      <a href='https://test.stytch.com/v1/public/oauth/google/start?public_token=public-token-test-1881250c-afb6-4c09-b284-95592cf2fd62' target='_blank'>
-        <Button>Log In With Google</Button>
-      </a>
-      <a className='pl-3' href='https://test.stytch.com/v1/public/oauth/github/start?public_token=public-token-test-1881250c-afb6-4c09-b284-95592cf2fd62' target='_blank'>
-        <Button>Log In With Github</Button>
-      </a>
-    </div>
-    <StytchLogin config={sdkConfig} styles={sdkStyle} callbacks={callbackConfig} />
+      <div className='w-1'>
+      <StytchLogin config={sdkConfig} styles={sdkStyle} callbacks={callbackConfig} />
+      </div>
+      <div className="relative my-5">
+        <div className="absolute inset-0 flex items-center">
+          <span className="w-full border-t" />
+        </div>
+        <div className="relative flex justify-center text-xs uppercase">
+          <span className="bg-background px-2 text-muted-foreground">
+            Or continue with
+          </span>
+        </div>
+      </div>
+      <Button
+        variant={"outline"}
+        onClick={() => {
+          window.open(getOauthUrl("github"), '_blank');
+        }}
+        className='mb-2'
+      >
+        <Icons.gitHub className="mr-2 h-4 w-4" />{" "}
+        Github
+      </Button>
+      <Button
+        variant={"outline"}
+        onClick={() => {
+          window.open(getOauthUrl("google"), '_blank');
+        }}
+      >
+        <Icons.google className="mr-2 h-4 w-4" />{" "}
+        Google
+      </Button>
     </>
   )
 }
