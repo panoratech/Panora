502603d37016fd5856549fcbc3ef65797dd1c550
mit-27 <meetsuthar64@gmail.com>
2024-04-29 00:52:27 -0400

    💚 Fix ci

--- packages/api/src/@core/auth/guards/jwt-auth.guard.ts
+++ packages/api/src/@core/auth/guards/jwt-auth.guard.ts
@@ -4,17 +4,16 @@ import {
     UnauthorizedException,
 } from '@nestjs/common';
 import { AuthGuard } from '@nestjs/passport';
+import { Observable } from 'rxjs';
 
 @Injectable()
 export class JwtAuthGuard extends AuthGuard('jwt') {
-    canActivate(context: ExecutionContext) {
-        // Add your custom authentication logic here
-        // for example, call super.logIn(request) to establish a session.
+
+    canActivate(context: ExecutionContext): boolean | Promise<boolean> | Observable<boolean> {
         return super.canActivate(context);
     }
 
-    handleRequest(err, user, info) {
-        // You can throw an exception based on either "info" or "err" arguments
+    handleRequest<TUser = any>(err: any, user: any, info: any, context: ExecutionContext, status?: any): TUser {
         if (err || !user) {
             throw err || new UnauthorizedException();
         }
