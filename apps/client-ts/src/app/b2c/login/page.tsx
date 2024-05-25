c1c4ecd8ddd87b064ae50f7ae84293a748c13e48
mit-27 <meetsuthar64@gmail.com>
2024-05-25 02:06:53 -0400

    🐛 Fix auth forms UI

--- apps/client-ts/src/app/b2c/login/page.tsx
+++ apps/client-ts/src/app/b2c/login/page.tsx
@@ -54,9 +54,9 @@ export default function Page() {
         {!userInitialized ? 
         (
             <div className='min-h-screen grid lg:grid-cols-2 mx-auto text-left'>
-                <div className='flex-1 flex flex-col justify-center py-12 px-4 sm:px-6 lg:flex-none lg:px-20 xl:px-24'>
+                <div className='flex-1 flex flex-col py-12 px-4 sm:px-6 lg:flex-none lg:px-20 xl:px-24'>
                     <img src="/logo.png" className='w-14' /> 
-                    <Tabs defaultValue="login" className="w-[400px]">
+                    <Tabs defaultValue="login" className="w-[400px] space-y-4">
                         <TabsList className="grid w-full grid-cols-2">
                             <TabsTrigger value="login">Login</TabsTrigger>
                             <TabsTrigger value="create">Create Account</TabsTrigger>
