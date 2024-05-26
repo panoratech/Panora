70309cd94f0f824a9d80e7dfabce87e15006fa15
mit-27 <meetsuthar64@gmail.com>
2024-05-26 17:10:53 -0400

    💄 Added success connection dialog for magic link

--- apps/magic-link/src/App.tsx
+++ apps/magic-link/src/App.tsx
@@ -5,7 +5,7 @@ import { ThemeProvider } from "@/components/theme-provider"
 function App() {
   return (
     <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
-      <div className="">
+      <div className="flex items-center justify-center">
         <ProviderModal/>
       </div>
     </ThemeProvider>
--- apps/magic-link/src/lib/ProviderModal.tsx
+++ apps/magic-link/src/lib/ProviderModal.tsx
@@ -1,6 +1,6 @@
 import { useEffect, useState } from 'react';
 import useOAuth from '@/hooks/useOAuth';
-import { findProviderByName, providersArray, categoryFromSlug, Provider } from '@panora/shared/src';
+import { findProviderByName, providersArray, categoryFromSlug, Provider,CONNECTORS_METADATA } from '@panora/shared/src';
 import { categoriesVerticals } from '@panora/shared/src/categories';
 import useLinkedUser from '@/hooks/queries/useLinkedUser';
 import useUniqueMagicLink from '@/hooks/queries/useUniqueMagicLink';
@@ -11,6 +11,8 @@ import { Button } from '@/components/ui/button';
 import { Select, SelectContent, SelectGroup, SelectItem, SelectLabel, SelectSeparator, SelectTrigger, SelectValue } from '@/components/ui/select';
 import { LoadingSpinner } from '@/components/ui/loading-spinner';
 import useProjectConnectors from '@/hooks/queries/useProjectConnectors';
+import {Unplug,X} from 'lucide-react'
+import Modal from '@/components/Modal';
 
 
 const LoadingOverlay = ({ providerName }: { providerName: string }) => {
@@ -48,6 +50,8 @@ const ProviderModal = () => {
   }>({status: false, provider: ''});
 
   const [uniqueMagicLinkId, setUniqueMagicLinkId] = useState('');
+  const [openSuccessDialog,setOpenSuccessDialog] = useState<boolean>(false);
+  const [currentProviderLogoURL,setCurrentProviderLogoURL] = useState<string>('')
 
   const {data: magicLink} = useUniqueMagicLink(uniqueMagicLinkId); 
   const {data: connectorsForProject} = useProjectConnectors(projectId);
@@ -90,10 +94,15 @@ const ProviderModal = () => {
   const { open, isReady } = useOAuth({
     providerName: selectedProvider?.provider!,
     vertical: selectedProvider?.category!,
-    returnUrl: "https://google.com", 
+    returnUrl: window.location.protocol + '//' + window.location.hostname + (window.location.port ? ':' + window.location.port : ''),
+    // returnUrl: "https://google.com", 
+    // returnUrl: window.location.hostname + (window.location.port ? ':' + window.location.port : ''),
     projectId: projectId,
     linkedUserId: magicLink?.id_linked_user as string,
-    onSuccess: () => console.log('OAuth successful'),
+    onSuccess: () => {
+      console.log('OAuth successful');
+      setOpenSuccessDialog(true);
+    },
   });
 
   const onWindowClose = () => {
@@ -124,6 +133,8 @@ const ProviderModal = () => {
   
   const handleWalletClick = (walletName: string, category: string) => {
     setSelectedProvider({provider: walletName.toLowerCase(), category: category.toLowerCase()});
+    const logoPath = CONNECTORS_METADATA[category.toLowerCase()][walletName.toLowerCase()].logoPath;
+    setCurrentProviderLogoURL(logoPath)
     setPreStartFlow(true);
   };
 
@@ -158,6 +169,7 @@ const ProviderModal = () => {
 
 
   return (
+    <>
     <Card className='w-[50vw]'>
       <CardHeader>
         <CardTitle className='flex flex-row items-start mb-2'>Connect to your software</CardTitle>
@@ -209,6 +221,27 @@ const ProviderModal = () => {
         {loading.status ? <Button className='w-full flex flex-row items-center' disabled><LoadingSpinner className="w-4 mr-2"/>Loading</Button> : <Button className="w-full" onClick={handleStartFlow} disabled={!preStartFlow}>Continue</Button>}
       </CardFooter>
     </Card>
+
+    {/* OAuth Successful Modal */}
+    <Modal open={openSuccessDialog} setOpen={setOpenSuccessDialog} >
+    <div className='h-[12rem] w-[20rem] justify-center flex p-1'>
+            <div className='flex flex-col gap-2 items-center'>
+            <div className='flex h-1/3 items-center justify-center gap-2'>
+            <Unplug className={`${openSuccessDialog ? "scale-100 opacity-100" : "scale-50 opacity-0"} transition-all duration-700`} size={60} color='#4CAF50' />
+            <X size={25} className={`${openSuccessDialog ? "opacity-100" : "opacity-0"} transition-all duration-500 delay-200`} color='gray' />
+            
+            <img className={`w-12 h-12 transition-all duration-700 delay-200 rounded-lg ml-3 ${openSuccessDialog ? "scale-100 opacity-100" : "scale-50 opacity-0"}`} src={currentProviderLogoURL} alt={selectedProvider?.provider} />
+            
+            </div>
+
+            <div className={`text-white transition-all ease-in delay-200 ${openSuccessDialog ? "opacity-100 scale-100" : "opacity-0 scale-125"} font-semibold text-xl items-center`}>Connected !</div>
+
+            <div className={`text-sm transition-all ease-in delay-200 ${openSuccessDialog ? "opacity-100 scale-100" : "opacity-0 scale-125"} text-gray-400 items-center align-middle text-center`}>The connection was successfully established. You can visit the Dashboard and verify the status.</div>
+
+            </div>
+    </div>
+    </Modal>
+    </>
   );
 };
 
