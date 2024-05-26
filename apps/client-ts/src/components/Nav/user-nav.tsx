ffd24bb7141a5deb7b5b6fe05b56f735d2b6c101
mit-27 <meetsuthar64@gmail.com>
2024-05-26 02:40:18 -0400

    💄 Improve dashboard UI

--- apps/client-ts/src/components/Nav/user-nav.tsx
+++ apps/client-ts/src/components/Nav/user-nav.tsx
@@ -6,25 +6,36 @@ import {
 import { Button } from "@/components/ui/button"
 import {
   DropdownMenu,
+  DropdownMenuCheckboxItem,
   DropdownMenuContent,
   DropdownMenuGroup,
   DropdownMenuItem,
   DropdownMenuLabel,
+  DropdownMenuPortal,
   DropdownMenuSeparator,
+  DropdownMenuSub,
+  DropdownMenuSubContent,
+  DropdownMenuSubTrigger,
   DropdownMenuTrigger,
 } from "@/components/ui/dropdown-menu"
 import useProfileStore from "@/state/profileStore";
 import { useRouter } from "next/navigation";
 import Link from "next/link";
 import Cookies from 'js-cookie';
-import useProjectStore from "@/state/projectStore"
+import useProjectStore from "@/state/projectStore";
 import { useQueryClient } from '@tanstack/react-query';
+import {User,LogOut,SunMoon,Sun,Moon,Monitor} from 'lucide-react';
+import {DotsHorizontalIcon} from '@radix-ui/react-icons';
+import { useTheme } from "next-themes";
+
 
 export function UserNav() {
   const router = useRouter();
   const { profile, setProfile } = useProfileStore();
   const { setIdProject } = useProjectStore();
   const queryClient = useQueryClient();
+  const { setTheme,theme } = useTheme();
+  // const [currentTheme,SetCurrentTheme] = useState(theme)
 
   const onLogout = () => {
     router.push('/b2c/login')
@@ -36,37 +47,79 @@ export function UserNav() {
   return (
     <DropdownMenu>
       <DropdownMenuTrigger asChild>
-        <Button variant="ghost" className="relative h-8 w-8 rounded-full">
-          <Avatar className="h-8 w-8">
+        {/* <Button variant="ghost" className="relative h-8 w-8 rounded-full"> */}
+          <div className="flex p-3 hover:bg-accent items-center gap-4">
+          <Avatar className="h-8 w-8 rounded-full">
             <AvatarImage src="/avatars/01.png" alt="@shadcn" />
             <AvatarFallback>SC</AvatarFallback>
           </Avatar>
-        </Button>
+          <p className="text-sm">
+          {profile ? `${profile.first_name} ${profile.last_name}` : ""}
+          </p>
+          <DotsHorizontalIcon className="ml-auto mr-2"/>
+
+          </div>
+        {/* </Button> */}
       </DropdownMenuTrigger>
-      <DropdownMenuContent className="w-56 ml-10" align="end" forceMount>
-        <DropdownMenuLabel className="font-normal">
+      <DropdownMenuContent className="w-56" align="center" forceMount>
+        {/* <DropdownMenuLabel className="font-normal">
           <div className="flex flex-col space-y-1">
             <p className="text-xs leading-none text-muted-foreground">
             {profile ? profile.email || profile.first_name : "No profile found"}
             </p>
           </div>
-        </DropdownMenuLabel>
-        <DropdownMenuGroup>
+        </DropdownMenuLabel> */}
+        {/* <DropdownMenuGroup> */}
           <Link href={"/b2c/profile"}>
             <DropdownMenuItem>
-              Profile
+            <User className="mr-2 h-4 w-4" />
+            <span>Profile</span>
             </DropdownMenuItem>
           </Link>
+
+          <DropdownMenuSub>
+            <DropdownMenuSubTrigger>
+              <SunMoon className="mr-2 h-4 w-4" />
+              <span>Theme</span>
+            </DropdownMenuSubTrigger>
+            <DropdownMenuPortal>
+              <DropdownMenuSubContent>
+                <DropdownMenuCheckboxItem
+                checked={theme==="light"}
+                onCheckedChange={() => setTheme("light")}
+                >
+                  <Sun className="mr-2 h-4 w-4" />
+                  <span>Light</span>
+                </DropdownMenuCheckboxItem>
+                <DropdownMenuCheckboxItem
+                checked={theme==="dark"}
+                onCheckedChange={() => setTheme("dark")}
+                >
+                  <Moon className="mr-2 h-4 w-4" />
+                  <span>Dark</span>
+                </DropdownMenuCheckboxItem>
+                <DropdownMenuCheckboxItem
+                checked={theme==="system"}
+                onCheckedChange={() => setTheme("system")}
+                >
+                  <Monitor className="mr-2 h-4 w-4" />
+                  <span>System</span>
+                </DropdownMenuCheckboxItem>
+              </DropdownMenuSubContent>
+            </DropdownMenuPortal>
+          </DropdownMenuSub>
+
           {/*<DropdownMenuItem>
             Billing
           </DropdownMenuItem>
           <DropdownMenuItem>
             Settings
       </DropdownMenuItem>*/}
-        </DropdownMenuGroup>
+        {/* </DropdownMenuGroup> */}
         <DropdownMenuSeparator />
             <DropdownMenuItem onClick={() => onLogout()} >
-            Log out
+            <LogOut className="mr-2 h-4 w-4" />
+            <span>Log Out</span>
             </DropdownMenuItem>
       </DropdownMenuContent>
     </DropdownMenu>
