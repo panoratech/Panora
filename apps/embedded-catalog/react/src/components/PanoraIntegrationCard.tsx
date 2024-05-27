fe03eb654f1e13cc0433e6f82ec16bb8a85949cf
mit-27 <meetsuthar64@gmail.com>
2024-05-27 12:04:16 -0400

    🎨 Added success dialog and automatic window close on succeessful auth

--- apps/embedded-catalog/react/src/components/PanoraIntegrationCard.tsx
+++ apps/embedded-catalog/react/src/components/PanoraIntegrationCard.tsx
@@ -4,6 +4,8 @@ import { CONNECTORS_METADATA, ConnectorCategory } from '@panora/shared';
 import { Button } from './ui/button2';
 import { Card } from './ui/card';
 import { ArrowRightIcon } from '@radix-ui/react-icons';
+import {Unplug,X} from 'lucide-react'
+import Modal from './Modal';
 
 export interface ProviderCardProp {
   name: string;
@@ -15,22 +17,33 @@ export interface ProviderCardProp {
 }
 
 const PanoraIntegrationCard = ({name, category, projectId, returnUrl, linkedUserId, optionalApiUrl}: ProviderCardProp) => {
-    const [loading, setLoading] = useState(false)
+    const [loading, setLoading] = useState(false);
+    const [openSuccessDialog,setOpenSuccessDialog] = useState<boolean>(false);
     const [startFlow, setStartFlow] = useState(false);
+    const returnUrlWithWindow = (typeof window !== 'undefined') 
+    ? window.location.protocol + '//' + window.location.hostname + (window.location.port ? ':' + window.location.port : '') 
+    : '';
+
 
     const { open, isReady } = useOAuth({
       providerName: name.toLowerCase(),
       vertical: category.toLowerCase() as ConnectorCategory,
-      returnUrl: returnUrl,
+      returnUrl: returnUrlWithWindow,
+      // returnUrl: window.location.protocol + '//' + window.location.hostname + (window.location.port ? ':' + window.location.port : ''),
+      // returnUrl: returnUrl,
       projectId: projectId,
       linkedUserId: linkedUserId,
       optionalApiUrl: optionalApiUrl,
-      onSuccess: () => console.log('OAuth successful'),
+      onSuccess: () => {
+        console.log('OAuth successful');
+        setOpenSuccessDialog(true);
+      },
     });
   
     const onWindowClose = () => {
       setLoading(false);
-      return;
+      setStartFlow(false);
+      // return;
     }
   
     useEffect(() => {
@@ -78,6 +91,26 @@ const PanoraIntegrationCard = ({name, category, projectId, returnUrl, linkedUser
                 </div>
               </div>
             </Card>
+
+            {/* OAuth Successful Modal */}
+            <Modal open={openSuccessDialog} setOpen={setOpenSuccessDialog} >
+            <div className='h-[12rem] w-[20rem] justify-center flex p-1'>
+                    <div className='flex flex-col gap-2 items-center'>
+                    <div className='flex h-1/3 items-center justify-center gap-2'>
+                    <Unplug className={`${openSuccessDialog ? "scale-100 opacity-100" : "scale-50 opacity-0"} transition-all duration-700`} size={60} color='#4CAF50' />
+                    <X size={25} className={`${openSuccessDialog ? "opacity-100" : "opacity-0"} transition-all duration-500 delay-200`} color='gray' />
+                    
+                    <img className={`w-12 h-12 transition-all duration-700 delay-200 rounded-lg ml-3 ${openSuccessDialog ? "scale-100 opacity-100" : "scale-50 opacity-0"}`} src={img} alt={name} />
+                    
+                    </div>
+
+                    <div className={`text-white transition-all ease-in delay-200 ${openSuccessDialog ? "opacity-100 scale-100" : "opacity-0 scale-125"} font-semibold text-xl items-center`}>Connection Successful!</div>
+
+                    <div className={`text-sm transition-all ease-in delay-200 ${openSuccessDialog ? "opacity-100 scale-100" : "opacity-0 scale-125"} text-gray-400 items-center align-middle text-center`}>The connection was successfully established. You can visit the Dashboard and verify the status.</div>
+
+                    </div>
+            </div>
+            </Modal>
       </div>
     )
   };
--- apps/magic-link/src/hooks/useOAuth.ts
+++ apps/magic-link/src/hooks/useOAuth.ts
@@ -1,67 +1,89 @@
 import config from '@/helpers/config';
-import { useState, useEffect } from 'react';
-import { constructAuthUrl } from '@panora/shared/src/test';
+import { useState, useEffect, useRef } from 'react';
+import { ConnectorCategory, constructAuthUrl } from '@panora/shared/src/test';
 
 type UseOAuthProps = {
   clientId?: string;
   providerName: string;           // Name of the OAuth provider
-  vertical: string;
+  vertical: ConnectorCategory;    // Vertical (Crm, Ticketing, etc)
   returnUrl: string;              // Return URL after OAuth flow
   projectId: string;              // Project ID
   linkedUserId: string;           // Linked User ID
+  optionalApiUrl?: string;        // URL of the User's Server
   onSuccess: () => void;
 };
 
-const useOAuth = ({ providerName, vertical, returnUrl, projectId, linkedUserId, onSuccess }: UseOAuthProps) => {
+const useOAuth = ({ providerName, vertical, returnUrl, projectId, linkedUserId, optionalApiUrl, onSuccess }: UseOAuthProps) => {
   const [isReady, setIsReady] = useState(false);
+  const intervalRef = useRef<NodeJS.Timeout | null>(null);
+  const authWindowRef = useRef<Window | null>(null);
 
   useEffect(() => {
     // Perform any setup logic here
     setTimeout(() => setIsReady(true), 1000); // Simulating async operation
+
+    return () => {
+      // Cleanup on unmount
+      clearExistingInterval(false);
+      if (authWindowRef.current && !authWindowRef.current.closed) {
+        authWindowRef.current.close();
+      }
+    };
   }, []);
 
+  const clearExistingInterval = (clearAuthWindow: boolean) => {
+    if (clearAuthWindow && authWindowRef.current && !authWindowRef.current.closed) {
+      authWindowRef.current.close();
+    }
+    if (intervalRef.current !== null) {
+      clearInterval(intervalRef.current);
+      intervalRef.current = null;
+    }
+  };
 
   const openModal = async (onWindowClose: () => void) => {
-    try {
-      const apiUrl = config.API_URL;
-      const authUrl = await constructAuthUrl({
-        projectId, linkedUserId, providerName, returnUrl, apiUrl, vertical
-      });
-      if (!authUrl) {
-        throw new Error("Auth Url is Invalid " + authUrl);
-      }
-      const width = 600, height = 600;
-      const left = (window.innerWidth - width) / 2;
-      const top = (window.innerHeight - height) / 2;
-      const authWindow = window.open(authUrl as string, 'OAuth', `width=${width},height=${height},top=${top},left=${left}`);
+    const apiUrl = optionalApiUrl ? optionalApiUrl : config.API_URL!;
+    const authUrl = await constructAuthUrl({
+      projectId, linkedUserId, providerName, returnUrl, apiUrl, vertical
+    });
 
-      const interval = setInterval(() => {
-        try {
-          const redirectedURL = authWindow!.location.protocol + '//' + authWindow!.location.hostname + (authWindow!.location.port ? ':' + authWindow!.location.port : '');
-          console.log("Redirected URL auth 0Auth : ", redirectedURL, " Actual returnURL : ", returnUrl);
-          if (redirectedURL === returnUrl) {
-            clearInterval(interval);
-            authWindow!.close();
-            onSuccess();
-          }
-        } catch (e) {
-          // To handle cross-origin error
-          console.log(e)
-        }
-
-        if (authWindow!.closed) {
-          clearInterval(interval);
-          if (onWindowClose) {
-            onWindowClose();
-          }
-        }
-      }, 500);
-
-      return authWindow;
-    } catch (error) {
-      console.error('Failed to open OAuth window', error);
-      onWindowClose();  // Reset the loading state
+    if (!authUrl) {
+      throw new Error("Auth Url is Invalid " + authUrl);
     }
+
+    const width = 600, height = 600;
+    const left = (window.innerWidth - width) / 2;
+    const top = (window.innerHeight - height) / 2;
+    const authWindow = window.open(authUrl as string, 'OAuth', `width=${width},height=${height},top=${top},left=${left}`);
+    authWindowRef.current = authWindow;
+
+    clearExistingInterval(false);
+
+    const interval = setInterval(() => {
+      try {
+        const redirectedURL = authWindow!.location.protocol + '//' + authWindow!.location.hostname + (authWindow!.location.port ? ':' + authWindow!.location.port : '');
+        if (redirectedURL === returnUrl) {
+          onSuccess();
+          clearExistingInterval(true);
+        }
+
+      } catch (e) {
+        console.log(e)
+      }
+      if (!authWindow || authWindow.closed) {
+        if (onWindowClose) {
+          onWindowClose();
+        }
+        authWindowRef.current = null;
+        console.log("Clearing direct close interval")
+        clearExistingInterval(false);
+      }
+
+    }, 500);
+
+    intervalRef.current = interval;
+
+    return authWindow;
   };
 
   return { open: openModal, isReady };
