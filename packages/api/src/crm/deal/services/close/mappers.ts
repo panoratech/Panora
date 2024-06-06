50b402ef83bfed266cf5da393d5b5d52f799411d
Rajesh Jonnalagadda <rajeshjonnalagadda48@gmail.com>
2024-06-06 14:48:56 +0530

    🐛 Minor api key value issue
    change the key expected value to vlaue

--- packages/api/src/crm/deal/services/close/mappers.ts
+++ packages/api/src/crm/deal/services/close/mappers.ts
@@ -132,7 +132,7 @@ export class CloseDealMapper implements IDealMapper {
       remote_id: deal.id,
       name: deal.note,
       description: deal.note, // Placeholder if there's no direct mapping
-      amount: parseFloat(`${deal.expected_value || 0}`),
+      amount: parseFloat(`${deal.value || 0}`),
       //TODO; stage_id: deal.properties.dealstage,
       field_mappings,
       ...opts,
