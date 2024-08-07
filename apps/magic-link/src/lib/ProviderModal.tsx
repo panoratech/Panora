b48b551cc18b83add85d3e9152836863aef4958b
nael <ouldbelkacemnael@yahoo.fr>
2024-08-07 20:05:22 +0200

    :bug: Fix typo

--- apps/magic-link/src/lib/ProviderModal.tsx
+++ apps/magic-link/src/lib/ProviderModal.tsx
@@ -36,6 +36,10 @@ interface IBasicAuthFormData {
   [key : string]: string
 }
 
+const domainFormats: { [key: string]: string } = {
+  microsoftdynamicssales: 'YOURORGNAME.api.crm12.dynamics.com',
+};
+
 const ProviderModal = () => {
   const [selectedCategory, setSelectedCategory] = useState("All");
   const [selectedProvider, setSelectedProvider] = useState<{
@@ -506,7 +510,12 @@ const ProviderModal = () => {
           <form onSubmit={(e) => { e.preventDefault(); onDomainSubmit(); }}>
             <div className="grid gap-4 py-4">
               <div className="grid gap-2">
-                <Label className={errors.end_user_domain ? 'text-destructive' : ''}>Enter your domain for {`${selectedProvider?.provider.substring(0,1).toUpperCase()}${selectedProvider?.provider.substring(1)}`}</Label>
+                <Label className={errors.end_user_domain ? 'text-destructive' : ''}>
+                  Enter your domain for {`${selectedProvider?.provider.substring(0,1).toUpperCase()}${selectedProvider?.provider.substring(1)}`}
+                  {domainFormats[selectedProvider?.provider.toLowerCase()] && (
+                    <span className="text-sm text-gray-500"> (e.g., {domainFormats[selectedProvider?.provider.toLowerCase()]})</span>
+                  )}                
+                </Label>
                 <Input
                   className="flex h-10 w-full rounded-md border border-input bg-background px-3 py-2 text-sm ring-offset-background file:border-0 file:bg-transparent file:text-sm file:font-medium placeholder:text-muted-foreground focus-visible:outline-none  focus-visible:ring-ring focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50"
                   placeholder="Your domain"
