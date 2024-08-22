c08d89ec1babcf6c72515e1e4cd3186dd4a83e8f
Subin C <subin8899@gmail.com>
2024-08-22 15:48:35 +0530

    🐛 Removed returnurl in code example #674
    removed returnurl in code example #674

--- apps/webapp/src/components/Configuration/Catalog/CopySnippet.tsx
+++ apps/webapp/src/components/Configuration/Catalog/CopySnippet.tsx
@@ -50,7 +50,7 @@ export const CopySnippet = () => {
             name={"hubspot"}
             category={ConnectorCategory.Crm}
             projectId={"c9a1b1f8-466d-442d-a95e-11cdd00baf49"}
-            returnUrl={"https://acme.inc"}
+            optionalApiUrl={"https://acme.inc"}
             linkedUserId={"b860d6c1-28f9-485c-86cd-fb09e60f10a2"} 
           />`
         );
