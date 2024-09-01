2ca3d362e6534f524d25870480a69c4642869092
colexeco <evan.shoemaker0@gmail.com>
2024-09-01 16:11:52 -0700

    specify accounting return types in index

--- packages/api/src/@core/connections/accounting/services/wave_financial/index.ts
+++ packages/api/src/@core/connections/accounting/services/wave_financial/index.ts
@@ -6,62 +6,51 @@
 // import { DesunifyReturnType } from '@@core/utils/types/desunify.input';
 // import { OriginalContactOutput } from '@@core/utils/types/original/original.accounting';
 // import { ApiResponse } from '@@core/utils/types';
-// import { AccountingObject } from '@lib/@types/index'
-
-// export interface IContactService {
-//   addContact(
-//     contactData: DesunifyReturnType,
-//     linkedUserId: string
-//   ): Promise<ApiResponse<OriginalContactOutput>>;
-
-//   syncContacts(
-//     linkedUserId: string
-//   ): Promise<ApiResponse<OriginalContactOutput[]>>;
-// }
-
-// @Injectable()
-// export class WaveFinancial implements IContactService {
-//   constructor(
-//     private prisma: PrismaService,
-//     private logger: LoggerService,
-//     private cryptoService: EncryptionService,
-//     private registry: ServiceRegistry,
-//   ) {
-//     this.logger.setContext(
-//       CrmObject.contact.toUpperCase() + ':' + WaveFinancial.name,
-//     );
-//     this.registry.registerService('my3rdParty', this);
+// import { AccountingObject } from '../../../../../../../shared/src/standardObjects';
+// import { AccountingObjectInput } from '@@core/utils/types/original/original.accounting';
 
 // export interface IContactService {
 //     addContact(
 //       contactData: DesunifyReturnType,
 //       linkedUserId: string
 //     ): Promise<ApiResponse<OriginalContactOutput>>;
-
+  
 //     syncContacts(
 //       linkedUserId: string
 //     ): Promise<ApiResponse<OriginalContactOutput[]>>;
-//   }
+// }  
 
 // @Injectable()
 // export class WaveFinancial implements IContactService {
 //   constructor(
-//     private prisma: PrismaService,
-//     private logger: LoggerService,
-//     private cryptoService: EncryptionService,
-//     private registry: ServiceRegistry,
-//   ) {
+//         private prisma: PrismaService,
+//         private logger: LoggerService,
+//         private cryptoService: EncryptionService,
+//         private registry: ServiceRegistry,
+//     ) {
 //     this.logger.setContext(
 //         AccountingObject.contact.toUpperCase() + ':' + WaveFinancial.name,
 //     );
-//     this.registry.registerService('my3rdParty', this);
+//     this.registry.registerService('wave_financial', this);
 //   }
 //   async addContact(
-//     contactData: 3rdPartyContactInput,
+//     contactData: AccountingObjectInput,
 //     linkedUserId: string,
-//   ): Promise<ApiResponse<3rdPartyContactOutput>> {}
+//   ): Promise<ApiResponse<WaveFinancialContactOutput>> {}
 
 //   async syncContacts(
 //     linkedUserId: string,
-//   ): Promise<ApiResponse<3rdPartyContactOutput[]>> {}
+//   ): Promise<ApiResponse<WaveFinancialContactOutput[]>> {}
+
+//   async handleCallBack(
+
+//   ): Promise<ApiResponse<WaveFinancialContactOutput>> {}
+
+//   async passthrough(
+
+//   ): Promise<ApiResponse<3rdPartyContactOutput>> {}
+
+//   async constructPassthrough(
+
+//   ): Promise<ApiResponse<3rdPartyContactOutput>> {}
 // }
